ac4eee51db0c278e96942c2869f71e21e1631c86
AaronXc <aaronwilcox2000@gmail.com>
2020-09-29 15:32:59 -0300

    change directories to look in to ones that are on the server

--- generate-osd-vars.py
+++ generate-osd-vars.py
@@ -2,8 +2,12 @@ import subprocess
 # first attempt at iteration and running command line programs
 # look through a file and grep a line for "someString"
 #subprocess.run(["cd", "~/TestMyPy"])
-subprocess.run(["ls"])
+subprocess.run(['ls', "etc"])
 someString = input("enter some text to look for")
-if subprocess.run(["cat", "c:/users/Aaron Wilcox/TestMyPy/listOfStuff"]):
-    count = subprocess.run(["wc", "-l"])
+if subprocess.run(["ls", "/etc", "|", "grep", "someString", "|", "wc", "-l" ]):
+    count = subprocess.run(["cat", "/etc", "|", "grep", "someString", "|", "wc", "-l" ]):
     print("succesfully found %2.1i instances of that string"% (count))
+    
+else:
+        print("no instances of that text found")
+    
