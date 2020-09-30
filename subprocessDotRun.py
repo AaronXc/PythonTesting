5d729226d13111a30c5bec417cbe5bb3441e10eb
AaronXc <aaronwilcox2000@gmail.com>
2020-09-30 15:45:39 -0300

    add the file which will generate osd vars using python

--- subprocessDotRun.py
+++ subprocessDotRun.py
@@ -1,21 +1,24 @@
 import subprocess
 #
 # some more arguments to add to the kwargs keymap are listed below:
-#------------------------------------------------
+###########################################################################
 #, "|", "grep", message
 #"input": message
 #, "shell": True
 #, "stdin": subprocess.PIPE
 #, "universal_newlines": True
-#, "stdout": subprocess.PIPE
+#
+# "stderr": subprocess.PIPE
+
 message = input("this will be the grep pattern")
-kwargs={"stderr": subprocess.PIPE }
+kwargs={ }
 #subprocess.run(['cd'])
-subprocess.run(['ls', '/usr/bin/'], **kwargs)
-print(subprocess.run(['ls', '/usr/bin/', "|", "grep", message], **kwargs))
+grepThis = subprocess.run(['ls', '/usr/bin/'], stdout=subprocess.PIPE).stdout
+print(subprocess.run(['ls', '/usr/bin/']))
+grepResults = subprocess.run(["grep", message, grepThis], stdout=subprocess.PIPE).stdout
 #subprocess.run(["echo", message], **kwargs).stdout
 
-p1 = subprocess.Popen(["echo", message], **kwargs)
+#p1 = subprocess.Popen(["echo", message], **kwargs)
 #p1.communicate(input=message) 
-print(p1.communicate()) 
+#print(p1.communicate()) 
 
