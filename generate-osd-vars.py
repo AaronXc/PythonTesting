3921326f88ceb8d5b5fa8ab9aaeb5eabf62dd16c
AaronXc <aaronwilcox2000@gmail.com>
2020-09-30 17:21:30 -0300

    started the actual script to get osd vars.

--- generate-osd-vars.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-#show the HBA cards used, the chassis size, if the chassis is a hybrid or not,
-#if CAS is used or not, the lvm volumes
-
-import subprocess
-subprocess.run(["$ALIAS_DEVICE_PATH"])
