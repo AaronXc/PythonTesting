b39e51032bc6af3b7d3ebd97d9bad61011fd531a
AaronXc <aaronwilcox2000@gmail.com>
2020-10-06 16:25:41 -0300

    "first" version of generate-osd-vars in python.

--- changeDirectory.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-# attempt to change the present working directory
-# , shell = True, stdout = subprocess.PIPE
-
-#subprocess.run(['cd'])
-subprocess.run(['ls', '/etc'])
-print(subprocess.run(['ls', '/etc']))
-
-   
